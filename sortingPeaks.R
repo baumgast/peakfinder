07ed5e455fd863967b6efa25754f50535bdc48cf
baumgast <baumsen@gmail.com>
2012-07-19 16:27:51 +0200

    started sorting and analysis script for candidate events

---
+++ sortingPeaks.R
@@ -0,0 +1,8 @@
+#script to sort and further analyse the reported UpCand and DownCand events
+#
+#Input: UpCand
+#       DownCand
+#       distaMT1
+#       distaMT
+#
+#---------------------------------------------------------------------------
